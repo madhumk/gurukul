ff326be7c6a09f90b5deefff4a6246a8676d8839
mkmads <mkmads@yahoo.com>
2021-08-09 13:11:00 +0530

    Changes to test

--- hub/src/main/java/com/gurukul/hub/service/HubService.java
+++ hub/src/main/java/com/gurukul/hub/service/HubService.java
@@ -5,7 +5,12 @@ import org.slf4j.LoggerFactory;
 import dto.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import java.util.Date;
 
 @Service
 public class HubService {
@@ -15,13 +20,37 @@ public class HubService {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
-    String TOPIC_NAME = "message_topic";
-    public Message postMessage(Message message) {
+    String TOPIC_NAME = "message_topic3";
+    public Message postMessage(Message msg) {
 
-        System.out.println(message.toString());
+
+
+        return sendMessage(msg);
+    }
+
+    public Message sendMessage(Message message) {
+        long currentTime = System.currentTimeMillis();
+        message.setAcceptedTime(new Date(currentTime));
         logger.info(String.format("Message sent -> %s", message));
         String jsonString = new Gson().toJson(message);
-        this.kafkaTemplate.send(TOPIC_NAME, jsonString);
-        return message;
+        ListenableFuture<SendResult<String, String>> future =
+                this.kafkaTemplate.send(TOPIC_NAME, message.getSenderId(),jsonString);
+
+        future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                System.out.println("Sent message=[" + message +
+                        "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            }
+            @Override
+            public void onFailure(Throwable ex) {
+                System.out.println("Unable to send message=["
+                        + message + "] due to : " + ex.getMessage());
+            }
+        });
+
+
+    return  message;
     }
 }
